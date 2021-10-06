b41a19f3d04d404d72d24b039d0eeacd503b07c6
Spellkaze <spellkaze@live.com>
2021-10-05 21:09:01 -0500

    Servidor http base

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('App start');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hola server express');
+});
+
+app.listen(port, () => {
+	console.log(`Mi port ${port}`);
+});

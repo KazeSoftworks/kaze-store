a18b182407f2e48a4de1e1300c562ee0f94195c8
Spellkaze <spellkaze@live.com>
2021-10-06 20:38:18 -0500

    Middleware de validacion de todas las rutas

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const ProductsService = require('../services/product');
+const {
+	createProductSchema,
+	updateProductSchema,
+	getProductSchema,
+} = require('../schemas/product');
+const validatorHandler = require('../middlewares/validator');
 
 const router = express.Router();
 const service = new ProductsService();
@@ -9,32 +15,44 @@ router.get('/', async (req, res) => {
 	res.json(products);
 });
 
-router.get('/:id', async (req, res, next) => {
-	try {
-		const { id } = req.params;
-		const product = await service.findOne(id);
-		res.json(product);
-	} catch (error) {
-		next(error);
+router.get(
+	'/:id',
+	validatorHandler(getProductSchema, 'params'),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const product = await service.findOne(id);
+			res.json(product);
+		} catch (error) {
+			next(error);
+		}
 	}
-});
+);
 
-router.post('/', (req, res) => {
-	const body = req.body;
-	const newProduct = service.create(body);
-	res.status(201).json(newProduct);
-});
-
-router.patch('/:id', async (req, res, next) => {
-	try {
-		const { id } = req.params;
+router.post(
+	'/',
+	validatorHandler(createProductSchema, 'body'),
+	(req, res) => {
 		const body = req.body;
-		const product = await service.update(id, body);
-		res.json(product);
-	} catch (error) {
-		next(error);
+		const newProduct = service.create(body);
+		res.status(201).json(newProduct);
 	}
-});
+);
+
+router.patch(
+	'/:id',
+	validatorHandler(updateProductSchema, 'body'),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const body = req.body;
+			const product = await service.update(id, body);
+			res.json(product);
+		} catch (error) {
+			next(error);
+		}
+	}
+);
 
 router.delete('/:id', async (req, res, next) => {
 	try {
